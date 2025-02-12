4f4f44c70d2f8a113e45fec9ba70759bd522e863
JaeH-H <hwangjh7887@gmail.com>
2025-02-12 18:17:32 +0900

    add : filter를 사용한 예외 처리

---
+++ src/main/java/com/spring/basicjpa/filter/ExceptionCheckFilter.java
@@ -0,0 +1,20 @@
+package com.spring.basicjpa.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class ExceptionCheckFilter implements Filter {
+
+    // WAS -> Filter -> Servlet
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+        log.info("::: 필터 호출 URL: {}", requestURI);
+        chain.doFilter(request, response);
+    }
+}
