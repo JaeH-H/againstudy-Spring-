ab430ce3ca86e2fdb75ad5622b6e48d45c5765ed
JaeH-H <hwangjh7887@gmail.com>
2025-02-13 19:08:00 +0900

    fix : POST에 횡단관심사 추가

--- src/main/java/com/spring/basicjpa/controller/StudentController.java
+++ src/main/java/com/spring/basicjpa/controller/StudentController.java
@@ -8,10 +8,12 @@ import com.spring.basicjpa.exception.ApiResponse;
 import com.spring.basicjpa.service.CourseNotFoundException;
 import com.spring.basicjpa.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/students")
@@ -29,18 +31,27 @@ public class StudentController {
      * ResponseEntity<ApiResponse<DTO>> : ResponseEntity <-> ApiResponse <-> DTO
      */
     public ResponseEntity<ApiResponse<StudentCreateResponseDto>> createCourseAPI(@RequestBody StudentCreateRequestDto studentCreateRequestDto) {
-//        try {
+
+        long startTime = System.currentTimeMillis();
+
+        try{
+            //GlobalExceptionHandler를 통한 예외처리로 Controller에서는 성공만 신경쓰면 된다.
+            StudentCreateResponseDto response = studentService.createStudent(studentCreateRequestDto);
+            ApiResponse<StudentCreateResponseDto> apiResponse = ApiResponse.success(HttpStatus.CREATED, "created", response);
+            return new ResponseEntity<ApiResponse<StudentCreateResponseDto>>(apiResponse, HttpStatus.CREATED);
+        } finally {
+            long endTime = System.currentTimeMillis();
+            long executeTime = endTime - startTime;
+            log.info("::: 실행 시간 : {}ms", executeTime);
+        }
+
+//         try {
 //            StudentCreateResponseDto response = studentService.createStudent(studentCreateRequestDto);
 //            return new ResponseEntity<StudentCreateResponseDto>(response, HttpStatus.CREATED);
 //
 //        }catch (CourseNotFoundException e) {
 //            return new ResponseEntity<StudentCreateResponseDto>(HttpStatus.NOT_FOUND);
 //        }
-
-        //GlobalExceptionHandler를 통한 예외처리로 Controller에서는 성공만 신경쓰면 된다.
-        StudentCreateResponseDto response = studentService.createStudent(studentCreateRequestDto);
-        ApiResponse<StudentCreateResponseDto> apiResponse = ApiResponse.success(HttpStatus.CREATED, "created", response);
-        return new ResponseEntity<ApiResponse<StudentCreateResponseDto>>(apiResponse, HttpStatus.CREATED);
     }
 
     /**
