e459a46e38f9f755cad1f48b5fb671084e4416b4
JaeH-H <hwangjh7887@gmail.com>
2025-02-19 18:55:21 +0900

    fix : 트랜잭션 실습

--- src/main/java/com/spring/basicjpa/transaction/TransactionService2.java
+++ src/main/java/com/spring/basicjpa/transaction/TransactionService2.java
@@ -37,4 +37,37 @@ public class TransactionService2 {
             throw new Exception("예외발생");
         }
     }
+
+    @Transactional
+    public void processTransaction() {
+
+        //작업단위1 : 등록 프로세서
+        processRegistration();
+
+        //작업단위2 : 결제 프로세서
+        processPayment();
+
+        //작업단위3 : 로깅 프로세서
+        processLogging();
+
+    }
+
+    public void processRegistration() {
+        Course registerCourse = Course.createInitCourse("등록 프로세서");
+        courseRepository.save(registerCourse);
+    }
+
+    public void processPayment() {
+        Course registerCourse = Course.createInitCourse("결제 프로세스");
+        courseRepository.save(registerCourse);
+    }
+
+    public void processLogging() {
+        Course registerCourse = Course.createInitCourse("로깅 프로세서");
+        courseRepository.save(registerCourse);
+
+        if(true) {
+            throw new RuntimeException("예외발생");
+        }
+    }
 }
